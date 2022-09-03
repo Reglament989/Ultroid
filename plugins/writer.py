270f7432c11e495623de7dda62394dced06f210e
Reglament989 <dudecom@tuta.io>
2022-09-03 13:48:01 +0300

    Add cirilic font for writer

--- plugins/writer.py
+++ plugins/writer.py
@@ -72,7 +72,7 @@ async def writer(e):
     k = await e.eor(get_string("com_1"))
     img = Image.open("resources/extras/template.jpg")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("resources/fonts/assfont.ttf", 30)
+    font = ImageFont.truetype("resources/fonts/10150.ttf", 30)
     x, y = 150, 140
     lines = text_set(text)
     line_height = font.getsize("hg")[1]
