4adbc8bf9380ad59367cebe48be10bc5dfd4b895
Reglament989 <dudecom@tuta.io>
2022-09-03 14:11:03 +0300

    Fix revert bool

--- plugins/writer.py
+++ plugins/writer.py
@@ -73,7 +73,7 @@ async def writer(e):
     k = await e.eor(get_string("com_1"))
     img = Image.open("resources/extras/template.jpg")
     draw = ImageDraw.Draw(ImageOps.flip(img) if flipped else img)
-    flipped = !flipped
+    flipped = not flipped
     font = ImageFont.truetype("resources/fonts/10150.ttf", 30)
     x, y = 150, 140
     lines = text_set(text)
